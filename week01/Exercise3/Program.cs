7159f2fd8f93346a355722f6df885ff4ed0bfbac
Neron56 <Michaelshaw20004@gmail.com>
2026-01-05 18:38:45 -0700

    Week 1 Day 1 project work

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Number game. I pick a magic number between 1-100. You guess a number, i will tell you higher or lower until you correctly guess it!");
+        Random randomizer = new Random();
+        int number = randomizer.Next(1,100);
+        int guess = -1;
+        int guessCounter = 1;
+        while (guess != number)
+        {
+            Console.Write("What number do you guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            if (guess == number)
+            {
+                Console.WriteLine($"You guessed it! the number was {number}!");
+                Console.WriteLine($"It took you {guessCounter} Tries");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            guessCounter ++;
+        }
+
     }
 }
