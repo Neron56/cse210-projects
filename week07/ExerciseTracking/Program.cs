60b442d40d6eb40d8509858eef2f425c1c793277
Neron56 <Michaelshaw20004@gmail.com>
2026-02-16 21:10:05 -0700

    Added screenshot to Week 7 project

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -8,10 +8,10 @@ class Program
         List<Activity> activities = [];
         DateTime date1 = DateTime.Today;
         DateTime date2 = new DateTime(2026,2,15);
-        Running running1 = new Running(date1.ToString("d"),30,2);
-        Running running2 = new Running(date1.ToString("d"),30,2.2F);
         Biking biking1 = new Biking(date2.ToString("d"),30,9.5F);
         Swimming swimming1 = new Swimming(date2.ToString("d"),45,45);
+        Running running1 = new Running(date1.ToString("d"),30,2);
+        Running running2 = new Running(date1.ToString("d"),30,2.2F);
         activities.Add(biking1);
         activities.Add(swimming1);
         activities.Add(running1);
